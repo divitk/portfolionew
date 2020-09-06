77294473812846133a4a4722e383fa8b236fa068
Divit SANJAY KARMIANI <divit.sanjaykarmiani@amadeus.com>
2020-09-06 22:52:42 +0530

    Updates

---
+++ precache-manifest.b565225b9511cda2301af2467d5316c6.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e23328319fe9cb8aa3bb55c7828e523",
+    "url": "/portfolionew/index.html"
+  },
+  {
+    "revision": "7389fb65e255d5c24699",
+    "url": "/portfolionew/static/css/2.3b4c6705.chunk.css"
+  },
+  {
+    "revision": "c693583da26572d5a4e5",
+    "url": "/portfolionew/static/css/main.203a87ae.chunk.css"
+  },
+  {
+    "revision": "7389fb65e255d5c24699",
+    "url": "/portfolionew/static/js/2.2b4923a5.chunk.js"
+  },
+  {
+    "revision": "00e57b0fb68dfc51f7b575b0020aea20",
+    "url": "/portfolionew/static/js/2.2b4923a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c693583da26572d5a4e5",
+    "url": "/portfolionew/static/js/main.4ad76157.chunk.js"
+  },
+  {
+    "revision": "5d8fa384f6b259cc9eb9",
+    "url": "/portfolionew/static/js/runtime-main.549a939d.js"
+  },
+  {
+    "revision": "eb1866e3f4d276ce12dbc41452371723",
+    "url": "/portfolionew/static/media/DoToday.eb1866e3.jpg"
+  },
+  {
+    "revision": "55b335243961413dd59172608e6c6aff",
+    "url": "/portfolionew/static/media/Flatmate.55b33524.png"
+  },
+  {
+    "revision": "286bb3fec477312809f521eaa82a7865",
+    "url": "/portfolionew/static/media/Lyft.286bb3fe.jpg"
+  },
+  {
+    "revision": "656d39f04145de405504983e30ca9fc2",
+    "url": "/portfolionew/static/media/MeruVsOla2.656d39f0.jpg"
+  },
+  {
+    "revision": "980d8077a75283610db438555730803c",
+    "url": "/portfolionew/static/media/Onlinefurniture.980d8077.png"
+  },
+  {
+    "revision": "f8ca9a4d10637bfc209b3defa9ff7d05",
+    "url": "/portfolionew/static/media/Zomato.f8ca9a4d.jpeg"
+  },
+  {
+    "revision": "83260087c309cbce9bd7d12a3fc7e92a",
+    "url": "/portfolionew/static/media/devOps.83260087.png"
+  },
+  {
+    "revision": "e59ae6889a3605acfdc4f7e4db2f3866",
+    "url": "/portfolionew/static/media/e-toilet-in-india.e59ae688.jpg"
+  },
+  {
+    "revision": "b5a365d346c1929f1cbacc5277ccd10d",
+    "url": "/portfolionew/static/media/gmail.b5a365d3.png"
+  },
+  {
+    "revision": "b189df81abc1b79f34168a4df9e56ac5",
+    "url": "/portfolionew/static/media/headout.b189df81.png"
+  },
+  {
+    "revision": "6a55ef3689624bcb95486fa636c65431",
+    "url": "/portfolionew/static/media/linkedin 2.6a55ef36.png"
+  },
+  {
+    "revision": "448c8a59ab31ff04bb055c4caefa4d1c",
+    "url": "/portfolionew/static/media/logo1.448c8a59.png"
+  },
+  {
+    "revision": "51a6ca23025eb5d58b269ffcef6b4654",
+    "url": "/portfolionew/static/media/macaques.51a6ca23.png"
+  },
+  {
+    "revision": "151df821dfd164ba36fe8eeec211cb84",
+    "url": "/portfolionew/static/media/medium.151df821.png"
+  },
+  {
+    "revision": "0927581e090202c04ec490d9e9d68277",
+    "url": "/portfolionew/static/media/medium_b.0927581e.png"
+  },
+  {
+    "revision": "dabfcb203d1529e89629fc1605d02841",
+    "url": "/portfolionew/static/media/mine.dabfcb20.jpg"
+  },
+  {
+    "revision": "73094f7c089171bd90e74c89cc6d36ab",
+    "url": "/portfolionew/static/media/musicstreaming.73094f7c.png"
+  },
+  {
+    "revision": "ce3222a460e6c7a4e5827b6dd87e6619",
+    "url": "/portfolionew/static/media/redbus.ce3222a4.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/portfolionew/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/portfolionew/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/portfolionew/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "ace62b3c3cf841c32d9bb80e2bd991fd",
+    "url": "/portfolionew/static/media/userjourneymap.ace62b3c.png"
+  }
+]);
